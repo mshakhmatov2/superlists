07ea8ba9fb223da4357067fd806439d543314a90
mikhail shakhmatov <mshakhmatov@gmail.com>
2023-12-09 23:56:22 +0600

    Добавили списки для всех элементов

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 
+def on_delete():
+    pass
+
+
+class List(models.Model):
+    """Список пунктов"""
+    pass
+
+
 class Item(models.Model):
     """Элемент списка"""
     text = models.TextField(default="")
+
+    list = models.ForeignKey(List, default=None, on_delete=on_delete)
+
+
