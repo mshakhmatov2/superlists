7f0be5534690b3697fa3925f8bd22b03c7bc41ca
mikhail shakhmatov <mshakhmatov@gmail.com>
2023-12-30 09:11:11 +0600

    Перемещена база данных sqlite за пределы главного дерева исходного кода

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -20,7 +20,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
         staging_server = os.environ.get('STAGING_SERVER')
         if staging_server:
-            self.live_server_url = 'https://' + staging_server
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.refresh()
--- superlists/settings.py
+++ superlists/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '../database/db.sqlite3',
     }
 }
 
