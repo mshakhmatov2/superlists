a619a81583656f163dab20f9f7f4031343b03af7
mikhail shakhmatov <mshakhmatov@gmail.com>
2024-01-12 21:32:41 +0600

    Рефакторизовано представление списка для обработки POSTзапросов с новыми элементами

--- lists/urls.py
+++ lists/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path(r'new', views.new_list, name='new_list'),
     path(r'<int:list_id>/', views.view_list, name='view_list'),
-    path(r'<int:list_id>/add_item', views.add_item, name='add_item'),
 ]
