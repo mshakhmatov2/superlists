d644d47f3f0238dae8e13ec73fd15981ac6cc42f
mikhail shakhmatov <mshakhmatov@gmail.com>
2024-01-12 22:03:57 +0600

    Рефакторизованы/удалены жестко кодированные URL из шаблонов

--- lists/urls.py
+++ lists/urls.py
@@ -19,7 +19,6 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path(r'new', views.new_list, name='new_list'),
     path(r'<int:list_id>/', views.view_list, name='view_list'),
 ]
